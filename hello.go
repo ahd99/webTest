719b8345aeba81c07cb813f1aad04c13062d4f4e
ali <ali@aheydari.ir>
2023-07-16 13:50:30 +0330

    test2

--- hello.go
+++ hello.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"hd99/webTest/calc"
+
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Printf("Hello golang\n")
 	fmt.Printf("%s\n", quote.Hello())
+	fmt.Printf("%d\n", calc.Sum(5, 7))
 }
